21178840c8759d9d961ae88047816fd950dc6d7c
alexadriano2006-lab <alexadriano2006@gmail.com>
2026-06-03 01:40:03 +0000

    cadastro e movimento das peças:  NIVEL NOVATO

--- xadrez.c
+++ xadrez.c
@@ -11,11 +11,40 @@ int main() {
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
 
+     int bispo = 5; // declaro a constante bispo, limitando a 5 movimentos
+
+     while (bispo <= 5) // enquanto a condição for verdadeira o loop ira se repetir, até que encontre uma condição falsa e encerre o progama
+     {
+        printf("BISPO: %d casas na diagonal superior direita\n", bispo); //apresenta o movimento realizado e quantidade de casas movidas
+        bispo++; //incrementa a variavel para que o loop encontre a saida
+     }
+     
+
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
 
+    int torre = 5; // declaro a constante torre, limitando a 5 movimentos
+    do
+    {
+
+    printf("Torre: %d casas para a direita\n", torre); //apresenta o movimento realizado e quantidade de casas movidas 
+    torre++; //incrementa a variavel para que o loop encontre a saida
+
+    } while (torre <= 5); // enquanto a condição for verdadeira o loop ira se repetir, até que encontre uma condição falsa e encerre o progama
+    
+
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+
+    int rainha = 8;
+
+    for (rainha = 8; rainha <= 8; rainha++) // enquanto a condição for verdadeira, o loop ira se repetir incrementando, até que encontre uma condição falsa e encerre o progama
+    {
+    
+        printf("RAINHA: %d casas para a esquerda\n", rainha); //apresenta o movimento realizado e quantidade de casas movidas
+
+    }
+    
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
